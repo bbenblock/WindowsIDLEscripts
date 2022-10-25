6dcbe17ab57d4adf197d069057cc51a2802f4a9a
bbenblock <91435966+bbenblock@users.noreply.github.com>
2022-10-25 14:33:09 +0100

    batch wipe drives x 4

---
+++ drive wipe.bat
@@ -0,0 +1,19 @@
+(echo Rescan
+echo rescan
+echo List Disk
+echo online disk 1
+echo Select Disk 1
+echo clean
+echo online disk 2
+echo select disk 2
+echo clean
+echo online disk 3
+echo select disk 3
+echo clean
+echo online disk 4
+echo select disk 4
+echo clean
+)  | diskpart
+pause>nul
+
+goto start
